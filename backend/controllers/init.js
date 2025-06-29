638a8d767977d152d848e0f49c1945c01f451029
ghanashyam2003 <shubhambadgujar2003@gmail.com>
2025-06-29 21:47:59 +0530

    Successfully write init

--- backend/controllers/init.js
+++ backend/controllers/init.js
@@ -1,8 +1,26 @@
-// controllers/init.js
+const fs = require("fs").promises;
+const path = require("path");
 
-function initRepo() {
-  console.log("Init command called");
-  
+
+ async function initRepo() {
+  const repoPath = path.resolve(process.cwd(), ".git");
+  const commitsPath = path.join(repoPath, "commits");
+
+    try {
+        await fs.mkdir(repoPath, { recursive: true });
+        await fs.mkdir(commitsPath, { recursive: true });
+        await fs.writeFile(
+            path.join(repoPath, "config.json"), 
+            JSON.stringify({buket: process.env.S3_BUCKET })
+        );
+        console.log("Repository initialized successfully.");
+    } catch (err) {
+        if (err.code === 'EEXIST') {
+            console.log("Repository already exists.");
+            return;
+        }
+        
+    }   
 }
 
 module.exports = { initRepo };
